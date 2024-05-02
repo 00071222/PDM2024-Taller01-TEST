11023f20137beae2f88cf02ffea66e963f15e229
00071222 <00071222@uca.edu.sv>
2024-05-01 19:11:14 -0600

    Changes

--- app/src/main/java/com/shadow/taller01_test_1/MainActivity.kt
+++ app/src/main/java/com/shadow/taller01_test_1/MainActivity.kt
@@ -17,30 +17,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Taller01TEST1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Taller01TEST1Theme {
-        Greeting("Android")
-    }
+
 }
